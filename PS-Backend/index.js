f2710f48f31cbaf470a269f6eda36013c7a2f6bb
Harshitha MahadevMahadevi <shivsaktiharshitha13@gmail.com>
2025-07-09 17:18:26 +0530

    ParvatiammaShivappa , clone of Pstwetter_x

---
+++ PS-Backend/index.js
@@ -0,0 +1,19 @@
+const express = require("express") ;
+const app = express() ;
+const port = 333 ;
+const path = require("path") ;
+
+app.set("view engine" , "ejs") ;
+app.set("views" , path.join(__dirname ,"views")) ;
+
+app.use((req , res , next) => {
+    console.log(`ParvatiammaShivappa , request received`) ;
+    next() ;
+})
+
+app.get("/" , (req ,res) => {
+    res.render("home.ejs") ;
+})
+app.listen(port , () => {
+    console.log(`ParvatiammaShivappa , our server is listening on port no ${port}`) ;
+}) ;
