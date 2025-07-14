b8674eaa5015daa7c6d02a78cf780233d5ae2164
Harshitha MahadevMahadevi <shivsaktiharshitha13@gmail.com>
2025-07-15 00:06:40 +0530

    ParvatiammaShivappa we made some changes

--- PS-Backend/public/js/script.js
+++ PS-Backend/public/js/script.js
@@ -1,4 +1,5 @@
 let navBarElePS = document.querySelectorAll(".PSchild-3") ;
+let topNavElePS = document.querySelectorAll(".topNavElePS") ;
 
 for(let i = 0 ; i < navBarElePS.length ; i++) {
     navBarElePS[i].addEventListener("click" , () => {
@@ -8,5 +9,14 @@ for(let i = 0 ; i < navBarElePS.length ; i++) {
         });
         navBarElePS[i].children[0].style = "color : #000" ;
         navBarElePS[i].children[1].classList.add("PS-click-bold") ;
+    });
+}
+for(let i = 0 ; i < topNavElePS.length ; i++) {
+    topNavElePS[0].children[1].classList.remove("rm-ele-PS") ;
+    topNavElePS[i].addEventListener("click" , () => {
+        topNavElePS.forEach((ele) => {
+            ele.children[1].classList.add("rm-ele-PS") ;
+        });
+        topNavElePS[i].children[1].classList.remove("rm-ele-PS") ;
     });
 }
