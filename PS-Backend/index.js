16dc49246f7fc4dad3aab87c9b78fe7ff62c3485
Harshitha MahadevMahadevi <shivsaktiharshitha13@gmail.com>
2025-07-27 17:50:19 +0530

    ParvatiammaShivappa done with our own api now let us learn sql

--- PS-Backend/index.js
+++ PS-Backend/index.js
@@ -3,10 +3,13 @@ const app = express() ;
 const port = 333 ;
 const path = require("path") ;
 const twetterPSdata = require("../PSdata.json") ;
+const methodOverride = require("method-override") ;
 
 app.set("view engine" , "ejs") ;
 app.set("views" , path.join(__dirname ,"views")) ;
-app.use(express.static(path.join(__dirname , "public"))) ;
+app.use(express.static(path.join(__dirname + "/public"))) ;
+app.use(express.urlencoded( { urlencoded : true } )) ;
+app.use(methodOverride("_method")) ;
 
 app.use((req , res , next) => {
     console.log(`ParvatiammaShivappa , request received`) ;
@@ -17,9 +20,124 @@ app.get("/PSx.com/posts" , (req ,res) => {
     let data = twetterPSdata ;
     res.render("home.ejs" , { data }) ;
 });
-app.get("/PSx.com/posts/form" , (req , res) => {
-    res.render("../../PS-Frontend/updateEntirePostPS") ;
+app.get("/PSx.com/posts/new" , (req , res) => {
+    res.render("../../PS-Backend/views/createNewPostPS.ejs");
+}) ; 
+app.post("/PSx.com/posts" , (req , res) => {
+    let { username , postMessage , profilePicPS , postImage } = req.body ;
+    let id = username ;
+    let referedAs = "@" + id ; 
+    let image = postImage ;
+    twetterPSdata.forEach((PSdata) => {
+        const rmKey = Object.values(PSdata)[0] ;
+        if(rmKey.id === username) {
+            rmKey.posts.push({
+                                image,
+                                likes : 0,
+                                comments : 0
+                            }) ;
+        }
+        else if((rmKey.id != username) && (rmKey.id != "dogs") && (rmKey.id != "cats")) {
+            let newObjPS = {
+                username : {
+                    id,
+                    referedAs,
+                    profilePicPS,
+                    postMessage,
+                    posts : [
+                        {
+                            image,
+                            likes : 0,
+                            comments : 0
+                        }
+                    ]
+                }
+            } ;
+            twetterPSdata.push(newObjPS) ;
+        }
+    }) ;
+    res.redirect("http://localhost:333/PSx.com/posts") ;
+}) ;
+app.get("/PSx.com/posts/:username" , (req , res) => {
+    let { username } = req.params ;
+    let findId = twetterPSdata.find((PSdata) => {
+        return PSdata[username] ;
+    }) ;
+    console.log(findId) ;
+    res.render("showInDetail" , { findId }) ;
+}) ;
+app.get("/PSx.com/posts/:username/PSedit/PostMessage" , (req , res) => {
+    let { username } = req.params ;
+    let findId = twetterPSdata.find((PSdata) => {
+        return PSdata[username] ;
+    })
+    res.render("editPostMesPS" , { findId }) ;
+}) ;
+app.patch("/PSx.com/posts/:username/postMes" , (req , res) => {
+    console.log(req.body) ;
+    console.log(req.params) ;
+    let { username } = req.params ;
+    let PSnewPostMes  = req.body.postMessagePS ;
+    let findId = twetterPSdata.find((PSdata) => {
+        return PSdata[username] ;
+    }) ;
+    console.log(findId) ;
+    findId[username].postMessage = PSnewPostMes ;
+    res.redirect("/PSx.com/posts") ;
+}) ;
+app.get("/PSx.com/posts/:username/PSedit/PostImg" , (req , res) => {
+    let { username } = req.params ;
+    let findId = twetterPSdata.find((PSdata) => {
+        return PSdata[username] ;
+    }) ;
+    res.render("editPostImg" , { findId }) ;
+}) ;
+app.patch("/PSx.com/posts/:username/postImg" , (req , res) => {
+    let { username } = req.params ;
+    let { PSpostImg } = req.body ;
+    console.log(PSpostImg) ;
+    let findId = twetterPSdata.find((PSdata) => {
+        return PSdata[username] ;
+    }) ;
+    console.log(findId) ;
+    let arrOfPostsPS = findId[username].posts ;
+    let recentPosts = arrOfPostsPS[arrOfPostsPS.length - 1] ;
+    console.log(recentPosts) ;
+    recentPosts.image = PSpostImg ;
+    res.redirect("/PSx.com/posts") ;
 })
+app.get("/PSx.com/posts/update/:username" , (req , res) => {
+    let { username } = req.params ;
+    let findId = twetterPSdata.find((PSdata) => {
+        return PSdata[username] ;
+    }) ;
+    console.log(findId) ;
+    res.render("updateRadically" , { findId : findId[username] });
+}) ; 
+app.put("/PSx.com/posts/:username" , (req , res) => {
+    console.log(req.body) ;
+    let { username , postMessage , profilePicPS , postImage} = req.body ;
+    let findId = twetterPSdata.find((PSdata) => {
+        return PSdata[username] ;
+    }) ;
+    findId[username].id = username ;
+    findId[username].referedAs = "@" + username ;
+    findId[username].profilePicPS = profilePicPS ;
+    findId[username].postMessage = postMessage ;
+    let arrOfPostsPS = findId[username].posts ;
+    let recentPosts = arrOfPostsPS[arrOfPostsPS.length - 1] ;
+    console.log(recentPosts) ;
+    recentPosts.image = postImage ;
+    res.redirect("/PSx.com/posts") ;    
+}) ;
+app.delete("/PSx.com/posts/delete/:username" , (req , res) => {
+    let { username } = req.params ;
+    let newPSdata = twetterPSdata.filter((PSdata) => {
+        return !PSdata[username] ;
+    }) ;
+    let data = newPSdata ;
+    res.render("home" , { data }) ;
+}) ;
 app.listen(port , () => {
     console.log(`ParvatiammaShivappa , our server is listening on port no ${port}`) ;
 }) ;
--- PS-Backend/public/js/scriptPSnewData.js
+++ PS-Backend/public/js/scriptPSnewData.js
@@ -1,16 +1,33 @@
-let textareaPS = document.querySelector("#updateMessage") ;
-let inputPS = document.querySelector("#updatePostImg") ;
+// in refers to input
+let inUsernamePS = document.querySelector("#username") ;
+let inPostMesPS = document.querySelector("#postMessage") ;
+let inProfilePicPS = document.querySelector("#profilePicPS") ;
+let inPostImagePS = document.querySelector("#postImage") ;
+let form = document.querySelector("form") ;
 
-
-if(textareaPS.value) {
-    let postMesPS = textareaPS.value ;
-    if(inputPS.value) {
-        let postImgAddPS = inputPS.value ;
+form.addEventListener("submit" , (event) => {
+    event.preventDefault() ;
+    console.dir(event) ;
+    if(inUsernamePS.value) {
+        if(inPostMesPS.value) {
+            if(inProfilePicPS.value) {
+                if(inPostImagePS.value) {
+                    console.log(`ParvatiammaShivappa says , data is being proceeded to be stored`) ;
+                    form.submit() ;
+                }
+                else {
+                    console.log(`ParvatiammaShivappa says , please fill in the details , else the post will not contain any image`) ;
+                }
+            }
+            else {
+                alert(`ParvatiammaShivappa says , please fill in the details , else the profile image will be un-updated`) ;
+            }
+        }
+        else {
+            alert(`ParvatiammaShivappa says , please fill the Post Message , if not the post Message would be ${inPostMesPS.value}`) ;
+        }
     }
     else {
-        alert(`Please enter the Post image address`) ;
+        alert("ParvatiammaShiappa says , please enter the details , it is neccessary to create new posts") ;
     }
-}
-else {
-    alert(`Please enter the Post Message`) ;
-}
+});
