43a7c90524cf35c71d2d4be97e7ec3a032d597ea
Harshitha MahadevMahadevi <shivsaktiharshitha13@gmail.com>
2025-07-30 00:31:14 +0530

    ParvatiammaShivappa done with our own api now let us learn sql

--- PS-Backend/index.js
+++ PS-Backend/index.js
@@ -8,7 +8,7 @@ const methodOverride = require("method-override") ;
 app.set("view engine" , "ejs") ;
 app.set("views" , path.join(__dirname ,"views")) ;
 app.use(express.static(path.join(__dirname + "/public"))) ;
-app.use(express.urlencoded( { urlencoded : true } )) ;
+app.use(express.urlencoded( { extended : true } )) ;
 app.use(methodOverride("_method")) ;
 
 app.use((req , res , next) => {
